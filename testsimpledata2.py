ad0ec0c5b58d5e6bf2399a0757a352f537576d7f
James Michael DuPont <jamesmikedupont@googlemail.com>
2013-10-28 07:35:25 -0500

    adding garbase collection

--- testsimpledata2.py
+++ testsimpledata2.py
@@ -1,8 +1,23 @@
 import resource
 import os.path 
 from report import Report
+import sys
+import gc
+
+last_res = None
+
 def mem():
+    global last_res
+    current=resource.getrusage(resource.RUSAGE_SELF)
     print (resource.getrusage(resource.RUSAGE_SELF))
+    if last_res:
+        #resource.struct_rusage(ru_utime=44.232, ru_stime=0.516, ru_maxrss=963272, ru_ixrss=0, ru_idrss=0, ru_isrss=0, ru_minflt=242030, ru_majflt=8, ru_nswap=0, ru_inblock=72168, ru_oublock=0, ru_msgsnd=0, ru_msgrcv=0, ru_nsignals=0, ru_nvcsw=320, ru_nivcsw=361)
+        utime_diff  = current.ru_utime - last_res.ru_utime
+        stime_diff  = current.ru_stime - last_res.ru_stime
+        maxrss_diff = current.ru_maxrss - last_res.ru_maxrss
+        print ("utime %s stime %s maxrss %s" % (utime_diff ,stime_diff, maxrss_diff))
+
+    last_res=current
 
 r=Report()
 
@@ -10,8 +25,11 @@ r=Report()
 def process(count):
 
     fname = "data/simpledata_" + str(count) + ".py"
-    code = "import data.simpledata_" + str(count)  
-    code2 = "data.simpledata_" + str(count)  + ".load()"
+
+    module_name = "data.simpledata_" + str(count)  
+    code = "import " + module_name 
+    code_load = module_name + ".load"
+    call_code_load = code_load  + "()"
 
     if not os.path.exists( fname):
         print ("missing %s" % fname)
@@ -21,13 +39,22 @@ def process(count):
     exec code
     print code
     count = 0
-    print (code2)
-    for x in eval(code2):
+    print (call_code_load)
+    for x in eval(call_code_load):
         r.add(x)
         count = count + 1
     print (fname, count)
 
-    
+    del x
+    del sys.modules[module_name]
+    del sys.modules["data"]
+    #exec("del %s" % code_load)
+    #print ("modules %s" % sorted(sys.modules.keys()))
+    #print (dir () )
+    #print (globals ())
+    #print (locals () )
+    gc.collect()
+
 for x in range(1,100):
     try :
         process(x)
